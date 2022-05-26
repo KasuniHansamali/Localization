6b8cd13f66ec96c08740b19cfbcdcd9be4866cda
Kasuni Hansamali <kasuni@mislholdings.com>
2022-05-26 11:19:31 +0530

    text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:localization/classes/language.dart';
+// import 'package:localization/classes/language.dart';
 import 'package:localization/localization/language_constants.dart';
 import 'package:localization/main.dart';
 
@@ -29,38 +30,56 @@ class _HomePageState extends State<HomePage> {
       ),
 
       body:
-          Container(
-            child: Center(
-                child:
-                DropdownButton<Language>(
-                  icon: Icon(
-                    Icons.language,
-                    color: Colors.black,
-                  ),
-                  hint: Text(getTranslated(context, 'change_language')?? ""),
-                  onChanged: (Language? language) {
-                    _changeLanguage(language);
-                  },
-                  items: Language.languageList()
-                      .map<DropdownMenuItem<Language>>(
-                        (e) => DropdownMenuItem<Language>(
-                      value: e,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Text(
-                            e.flag,
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          Text(e.name)
-                        ],
+          Column(
+            children: [
+              Container(
+                child: Center(
+                    child:
+                    DropdownButton<Language>(
+                      icon: Icon(
+                        Icons.language,
+                        color: Colors.black,
                       ),
+                      hint: Text(getTranslated(context, 'change_language')?? ""),
+                      onChanged: (Language? language) {
+                        _changeLanguage(language);
+                      },
+                      items: Language.languageList()
+                          .map<DropdownMenuItem<Language>>(
+                            (e) => DropdownMenuItem<Language>(
+                          value: e,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Text(
+                                e.flag,
+                                style: TextStyle(fontSize: 30),
+                              ),
+                              Text(e.name)
+                            ],
+                          ),
+                        ),
+                      )
+                          .toList(),
                     ),
-                  )
-                      .toList(),
-                )
-            ),
-          )
+
+                ),
+
+
+              ),
+              Container(
+                child: Center(
+                  child:Text(
+                      getTranslated(
+                          context, 'create_your_new_account_for_future_uses.')?? ""),
+                ),
+
+
+              ),
+            ],
+          ),
+
+
       );
 
   }
